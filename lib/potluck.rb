b63d186e872fd5bce658beece6931b2a02582a0d
Matt Levy <milevy1@gmail.com>
2019-01-24 08:28:04 -0700

    Added putluck.menu and test

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -14,4 +14,20 @@ class Potluck
     @dishes.select{ |dish| dish.category == category }
   end
 
+  def menu
+    menu_hash = {}
+    # Group dishes by category
+    menu_hash_temporary = @dishes.group_by { |dish| dish.category }
+    # Convert to form of just :category => dish.name
+    # Store in menu_hash
+    menu_hash_temporary.each { |key, value|
+      plural_category = (key.to_s + "s").to_sym
+      menu_hash[plural_category] = value.map { |dish| dish.name }
+    }
+    # Sort dish.names alphabetically
+    menu_hash.each { |key, value|
+      value.sort!
+    }
+  end
+
 end
