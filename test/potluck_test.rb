362c7f941b1a9263938fb4cd5e341e4a2408a8ef
Matt Levy <milevy1@gmail.com>
2019-01-23 21:46:23 -0700

    Added test for dishes length of array

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -27,4 +27,14 @@ class PotluckTest < Minitest::Test
     assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes
   end
 
+  def test_potluck_dishes_length_is_2
+    potluck = Potluck.new("7-13-18")
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
+
+    assert_equal 2, potluck.dishes.length
+  end
+
 end
